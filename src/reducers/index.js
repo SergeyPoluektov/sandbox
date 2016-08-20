a9aaf79833da1d52dc6cc884b6b4580bdb2d568c
Sergey Poluektov <spoluer@gmail.com>
2016-08-20 04:35:53 +0300

    added stuff to ignore

--- src/reducers/index.js
+++
@@ -1,6 +0,0 @@
-import {combineReducers} from 'redux'
-import departure from '../Departure/ducks/departure'
-
-export default combineReducers({
-	departure
-})
--- src/sagas/index.js
+++
@@ -1,6 +0,0 @@
-import {call} from 'redux-saga/effects'
-import departureSage from '../Departure/sagas'
-
-export default function* mainSaga() {
-	yield [call(departureSage)];
-}
